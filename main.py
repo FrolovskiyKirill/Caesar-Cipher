37c63753b5fd1effa7b430f6714ddb9c148918bb
Kirill Frolovskiy <97293194+FrolovskiyKirill@users.noreply.github.com>
2022-06-21 10:11:02 +0000

    final version

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+from art import logo
+print(logo)
 
 def ceasar(start_text, shift_amount, cipher_direction):
   converted_text = ""
   if cipher_direction == 'decode':
     shift_amount *= -1
-  for letter in text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    converted_text += alphabet[new_position]
+  for char in start_text:
+    if char not in alphabet:
+      converted_text += char
+    elif char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      converted_text += alphabet[new_position]
   print(f"The {cipher_direction}d text is {converted_text}")
 
-ceasar(start_text=text, shift_amount=shift, cipher_direction=direction)
+should_continue = True
+
+while should_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  
+  shift = shift % len(alphabet)
+  
+  ceasar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to restart, type 'no' if you want to exit.\n")
+
+  if restart == "no":
+    should_continue = False
+    print("Goodbye!")
